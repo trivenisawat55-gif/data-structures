f7d9fe904d1831cd49d7e893db1ce3d520ef1336
TRIVENI SAWAT <trivenisawat55@gmail.com>
2026-02-18 10:16:32 +0530

    Create array1.c
    taking inputs for array then printing it

---
+++ array1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//printing array from 1 to 10
+int main(){
+	int a[10],n,i;
+	printf("enter any number");
+   for( i=0;i<10;i++){
+   	scanf("%d",&a[i]);
+  }
+	for( i=0;i<10;i++){
+		
+		printf("%d",a[i]);
+	}
+
+return 0;	
+}
